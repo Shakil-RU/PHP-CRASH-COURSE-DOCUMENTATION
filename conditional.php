61069c4c9fff3020994364a469844e1114843e2c
shakil hossan <shakilhossan113@gmail.com>
2023-11-11 13:38:40 +0600

    updated file

--- conditional.php
+++ conditional.php
@@ -1,14 +1,46 @@
- <?php
- $age = 20;
+<?php
+echo "Before the conditional.";
+if (4 > 3) {
+ echo "Inside the conditional.";
+}
+if (3 > 4) {
+ echo "This will not be printed.";
+}
+echo "After the conditional.";
 
- if($age >= 18)
- {
-    echo 'You are old enough to vote';
- }
- else{
-    echo 'Soory! , You are not old enough to vote ';
- }
+echo "</br>";
 
 
+// another example of conditional.
 
- ?>
+if (4 > 5) {
+   echo "Not printed";
+  } elseif (4 > 4) {
+   echo "Not printed";
+  } elseif (4 == 4) {
+   echo "Printed.";
+  } elseif (4 > 2) {
+   echo "Not evaluated.";
+  } else {
+   echo "Not evaluated.";
+  }
+  if (4 == 4) {
+   echo "Printed";
+  }
+
+  // Switch…case.
+
+  $title = 'Twilight';
+switch ($title) {
+ case 'Harry Potter':
+ echo "Nice story, a bit too long.";
+ break;
+ case 'Twilight':
+ echo 'Uh...';           // break is missing so printed up to end.
+ case 'Lord of the Rings':  
+ echo "A classic!";
+ default:
+ echo "Dunno that one.";
+}
+
+?>
