44b9f40a1068164dfc1dae32ae2c63e2ff04ad8b
shakil hossan <shakilhossan113@gmail.com>
2023-11-11 14:44:02 +0600

    loop.php updated

--- loop.php
+++ loop.php
@@ -1,10 +1,46 @@
+
 <?php
-for($i = 0; $i<10; $i++)
-{
-    echo 'Number ' . $i . '</br>';
+//while loop
+$i = 0;
+while ($i < 4) {
+ echo $i . " ";
+ $i++;
 }
 
-$post = ['shail', 'arif', 'joy', 'bangle'];
-foreach($post as $it)  
-echo $it;
+//do...while
+echo "</br>";
+echo "with while: ";
+$i = 1;
+while ($i < 0) {
+ echo $i . " ";
+ $i++;
+}
+echo "with do-while: ";
+$i = 1;
+do {
+ echo $i . " ";
+ $i++;
+} while ($i < 0);
+
+//for
+echo "</br>";
+for ($i = 1; $i < 10; $i++) {
+    echo $i . " ";
+   }
+   echo "</br>";
+   $names = ['Harry', 'Ron', 'Hermione'];
+  for ($i = 0; $i < count($names); $i++) {
+  echo $names[$i] . " ";
+}
+
+//foreach
+echo "</br>";
+$names = ['Harry', 'Ron', 'Hermione'];
+foreach ($names as $name) {
+ echo $name . " ";
+}
+echo "</br>";
+foreach ($names as $key => $name) {
+ echo $key . " -> " . $name . " ";
+}
 ?>
